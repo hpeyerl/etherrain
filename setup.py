7e662a0ada80f7de09512449b8066512c2d8248d
Herb Peyerl <hpeyerl+gitlab@beer.org>
2017-08-29 06:24:42 -0600

    first attempt at pypi readiness

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'etherrain',
+  packages = ['etherrain'],
+  version = '0.1-dev0',
+  description = 'Interface Library for QuickSmart Etherrain/8 irrigation controller',
+  author = 'Herb Peyerl',
+  author_email = 'hpeyerl+etherrain@beer.org',
+  url = 'https://github.com/hpeyerl/etherrain',
+  license='MIT',
+  download_url = 'https://github.com/hpeyerl/etherrain/archive/0.1-dev0.tar.gz',
+  keywords = ['irrigation', 'watering'],
+  classifiers = [
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
+  ]
+)
+
